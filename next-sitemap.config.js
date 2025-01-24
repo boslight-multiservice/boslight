b6535bbb274668cea0f2c6e5145f32156147eb4d
Tolushawlar <Tolushaw.it@gmail.com>
2025-01-24 10:21:00 +0100

    feature: sitemap generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://www.boslightmulti-serviceslimited.com', // Replace with your website URL
+    generateRobotsTxt: true, // Generates robots.txt file
+    sitemapSize: 5000, // Adjust if you have many pages
+  };
+  
